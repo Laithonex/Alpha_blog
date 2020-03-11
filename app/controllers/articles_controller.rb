680ca07f034d9b0fa1cc070352fbccacd900263b
Laithonex <laith.aljufaili966@gmail.com>
2020-03-12 02:39:46 +0400

    Add Edit, update, show, and index listing

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,6 +1,14 @@
 class ArticlesController < ApplicationController
+    def index
+        @Anything = Article.all
+    end 
+
     def new
         @article = Article.new
+    end
+
+    def edit
+        @article = Article.find(params[:id])
     end
 
     def create
@@ -16,9 +24,20 @@ class ArticlesController < ApplicationController
         # redirect_to article_path(@article)
     end
 
+    def update
+        @article = Article.find(params[:id])
+        if @article.update(article_params)
+            flash[:notice] = "Article was seccessfully updated"
+            redirect_to article_path(@article)
+        else
+            render 'edit'
+        end
+    end
+
     def show
         @article = Article.find(params[:id])
     end 
+
     private
     def article_params
         params.require(:article).permit(:title, :discription)
