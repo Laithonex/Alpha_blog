e23d91f63a0aa5b3ef110c99809eeff132138a6a
Laithonex <laith.aljufaili966@gmail.com>
2020-03-12 03:37:27 +0400

    add partials and destroy Action

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -38,6 +38,14 @@ class ArticlesController < ApplicationController
         @article = Article.find(params[:id])
     end 
 
+    def destroy
+        @article = Article.find(params[:id])
+        @article.destroy
+        flash[:notice] = "Article was seccessfully deleted"
+        redirect_to article_path(@article)
+
+    end
+
     private
     def article_params
         params.require(:article).permit(:title, :discription)
