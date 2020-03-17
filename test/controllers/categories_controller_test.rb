0a59c3e3ad0bd8d8696c2ef33bade057ddc71bb8
Laithonex <laith.aljufaili966@gmail.com>
2020-03-18 01:48:15 +0400

    update test and Navebar

--- test/controllers/categories_controller_test.rb
+++ test/controllers/categories_controller_test.rb
@@ -3,9 +3,12 @@ require 'test_helper';
 class CategoriesControllerTest < ActionDispatch::IntegrationTest
 
     # def setup
-    #     @category = Category.create(name: "sports")    
+    #     # @category = Category.create(name: "sports")    
+    #     @user = User.create(username: "john", email:"john@example.com", password:"password", admin:"true")
     # end
-    
+    def sign_in_as(user, password)
+        post login_path, params: { session: { email: user.email, password: password } }
+    end 
     test "should get categories Index" do
         get categories_path
         assert_response :success
@@ -16,5 +19,12 @@ class CategoriesControllerTest < ActionDispatch::IntegrationTest
         assert_response :success
 
     end
+
+    test "should redirect creat when admin not logged in" do 
+        assert_no_difference 'Category.count' do
+        post categories_path, params:{category:{name:"sports"}}  
+        end 
+        assert_redirected_to categories_path
+    end 
 
 end 
--- test/integration/create_categories_test.rb
+++ test/integration/create_categories_test.rb
@@ -3,7 +3,12 @@ require 'test_helper';
 
 class CreateCategoriesTest < ActionDispatch::IntegrationTest
 
+    def setup
+        post login_path, params: { session: { email: user.email, password: password } }
+    end
+
     test "get new category form and create category" do
+    sign_in_as(@user,"password")
     get new_category_path             
     assert_template 'categories/new'    
         assert_difference 'Category.count', 1 do      
@@ -15,6 +20,7 @@ class CreateCategoriesTest < ActionDispatch::IntegrationTest
     end 
 
     test "invalid Category result in failure" do 
+        sign_in_as(@user,"password")
         get new_category_path             
         assert_template 'categories/new'    
         assert_no_difference 'Category.count' do      
